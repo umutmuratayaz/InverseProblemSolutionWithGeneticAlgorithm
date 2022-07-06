a141fb3e2fcb0778a59ae90d6c156582056078bb
Umut Murat Ayaz <88974388+umutmuratayaz@users.noreply.github.com>
2022-07-06 04:14:50 +0300

    Add files via upload

---
+++ MonoSol/gaCheck.m
@@ -0,0 +1,18 @@
+
+%% Function of Checking whether the population is in search space.
+
+function [pop] = gaCheck(pop,Gen,RoUp,ThiUp,RoDown,ThiDown,Chr)
+    for i=1:Chr
+        for ij=1:ceil(Gen/2)
+            if pop(i,ij)>RoUp || pop(i,ij)<RoDown
+                pop(i,ij)=unifrnd(RoDown,RoUp);
+            end
+        end 
+        for ik=ceil(Gen/2)+1:Gen
+            if pop(i,ik)>ThiUp || pop(i,ik)<ThiDown
+                pop(i,ik)=unifrnd(ThiDown,ThiUp);
+            end
+        end 
+    end
+end
+
